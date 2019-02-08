c140a7ddc735b2a0744f39b348f4f048917a3d7e
omidekz <40366040+omidekz@users.noreply.github.com>
2019-02-08 12:36:38 +0330

    Update Main.java

--- codes/Main.java
+++ codes/Main.java
@@ -3,10 +3,11 @@
  * @author mdsinalpha
  */
 public class Main 
+    private static final String TEAM_NAME = "ABSANT"; //Write your team name here
 {
     public static void main(String[] args) {
         Game game = new Game("192.168.62.82",9595);
         if(game.connect_to_server())
-            game.start("abSANT"); //Write your team name here
+            game.start(TEAM_NAME); 
     }
 }
